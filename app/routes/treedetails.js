4bf309c65fadff16baa0ad01501d56f0a99b781b
Alan Jiang <alanyimajiang@hotmail.com>
2016-09-16 22:45:47 -0400

    Home, About, Trees, and Treedetails are now working and use Noah's templates to look nice! Currently, the treedetails attributes images aren't setup, however, and the statistics links don't go anywhere.  Making the attributes work will require some more thought, coding, and discussion.

--- app/routes/treedetails.js
+++ app/routes/treedetails.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    afterModel: function(tree, transition) {
+    /*afterModel: function(tree, transition) {
         if (tree.get('Common_Name') === 'White Oak') {
             localStorage.treeOne = "true";
         }
         if (tree.get('Common_Name') === 'Japanese Maple') {
             localStorage.treeTwo = "true";
         }
-    }
+    }*/
 });
