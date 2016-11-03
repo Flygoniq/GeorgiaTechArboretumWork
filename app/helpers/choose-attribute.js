199509235cd011adadfd2336ca0ed5a89cd0e880
Alan Jiang <alanyimajiang@hotmail.com>
2016-11-03 00:01:01 -0400

    Attributes now use CSS

--- app/helpers/choose-attribute.js
+++ app/helpers/choose-attribute.js
@@ -1,177 +1,179 @@
 import Ember from 'ember';
-
+//class names come from app.css
 export function chooseAttribute(params, namedArgs) {
     var [value, store] = params;
+    var answer = "attribute";
     switch (value) {
         case "TRUE":
             if (namedArgs.number === "1") {
                 if (namedArgs.usage === "1") {return 1;}
-                return store.peekRecord('attribute', 1).get('Image_Path');
+                return answer + " ga";
+                //return store.peekRecord('attribute', 1).get('Image_Path');
             }
             else if (namedArgs.number === "3") {
                 if (namedArgs.usage === "1") {return 2;}
-                return store.peekRecord('attribute', 2).get('Image_Path');
+                return answer + " bee";
             }
             break;
         case "Medicine":
             if (namedArgs.usage === "1") {return 3;}
-            return store.peekRecord('attribute', 3).get('Image_Path');
+            return answer + " medicinal";
         case "Landscaping":
             if (namedArgs.usage === "1") {return 4;}
-            return store.peekRecord('attribute', 4).get('Image_Path');
+            return answer + " landscaping";
         case "Furniture":
             if (namedArgs.usage === "1") {return 5;}
-            return store.peekRecord('attribute', 5).get('Image_Path');
+            return answer + " furniture";
         case "Construction":
             if (namedArgs.usage === "1") {return 6;}
-            return store.peekRecord('attribute', 6).get('Image_Path');
+            return answer + " construction";
         case "Transportation":
             if (namedArgs.usage === "1") {return 7;}
-            return store.peekRecord('attribute', 7).get('Image_Path');
+            return answer + " transportation";
         case "Barrels":
             if (namedArgs.usage === "1") {return 8;}
-            return store.peekRecord('attribute', 8).get('Image_Path');
+            return answer + " barrel";
         case "Music":
             if (namedArgs.usage === "1") {return 9;}
-            return store.peekRecord('attribute', 9).get('Image_Path');
+            return answer + " music";
         case "Tools":
             if (namedArgs.usage === "1") {return 10;}
-            return store.peekRecord('attribute', 10).get('Image_Path');
+            return answer + " tools";
         case "Sports":
             if (namedArgs.usage === "1") {return 11;}
-            return store.peekRecord('attribute', 11).get('Image_Path');
+            return answer + " sports";
         case "Food":
             if (namedArgs.usage === "1") {return 12;}
-            return store.peekRecord('attribute', 12).get('Image_Path');
+            return answer + " food";
         case "Red":
             if (namedArgs.number === "4") {
                 if (namedArgs.usage === "1") {return 13;}
-                return store.peekRecord('attribute', 13).get('Image_Path');
+                return answer + " fallred";
             } else if (namedArgs.number === "8") {
                 if (namedArgs.usage === "1") {return 35;}
-                return store.peekRecord('attribute', 35).get('Image_Path');
+                return answer + " flowerred";
             } break;
         case "Orange":
             if (namedArgs.usage === "1") {return 14;}
-            return store.peekRecord('attribute', 14).get('Image_Path');
+            return answer + " fallorange";
         case "Yellow":
             if (namedArgs.number === "4") {
                 if (namedArgs.usage === "1") {return 15;}
-                return store.peekRecord('attribute', 15).get('Image_Path');
+                return answer + " fallyellow";
             } else if (namedArgs.number === "8") {
                 if (namedArgs.usage === "1") {return 37;}
-                return store.peekRecord('attribute', 37).get('Image_Path');
+                return answer + " floweryellow";
             } break;
         case "Copper":
             if (namedArgs.usage === "1") {return 16;}
-            return store.peekRecord('attribute', 16).get('Image_Path');
+            return answer + " fallcopper";
         case "Green":
             if (namedArgs.number === "4") {
                 if (namedArgs.usage === "1") {return 17;}
-                return store.peekRecord('attribute', 17).get('Image_Path'); 
+                return answer + " fallgreen";
             } else if (namedArgs.number === "8") {
                 if (namedArgs.usage === "1") {return 36;}
-                return store.peekRecord('attribute', 36).get('Image_Path');
+                return answer + " flowergreen";
             } break;
         case "Needle Leaf Conifers":
 			if (namedArgs.usage === "1") {return 18;}
-			return store.peekRecord('attribute', 18).get('Image_Path');
+            return answer + " needle";
         case "Scale Leaf Conifers":
 			if (namedArgs.usage === "1") {return 19;}
-			return store.peekRecord('attribute', 19).get('Image_Path');
+            return answer + " scale";
         case "Untoothed Simple":
 			if (namedArgs.usage === "1") {return 20;}
-			return store.peekRecord('attribute', 20).get('Image_Path');
+            return answer + " untoothed";
         case "Toothed Simple":
 			if (namedArgs.usage === "1") {return 21;}
-			return store.peekRecord('attribute', 21).get('Image_Path');
+            return answer + " toothed";
         case "Lobed Simple":
 			if (namedArgs.usage === "1") {return 22;}
-			return store.peekRecord('attribute', 22).get('Image_Path');
+            return answer + " lobed";
         case "Compound":
 			if (namedArgs.usage === "1") {return 23;}
-			return store.peekRecord('attribute', 23).get('Image_Path');
+            return answer + " compound";
         case "Yuccas and Palmettos":
 			if (namedArgs.usage === "1") {return 24;}
-			return store.peekRecord('attribute', 24).get('Image_Path');
+            return answer + " yucca";
         case "Alternate":
 			if (namedArgs.usage === "1") {return 25;}
-			return store.peekRecord('attribute', 25).get('Image_Path');
+            return answer + " alternate";
         case "Opposite":
 			if (namedArgs.usage === "1") {return 26;}
-			return store.peekRecord('attribute', 26).get('Image_Path');
+            return answer + " opposite";
         case "Whorled":
 			if (namedArgs.usage === "1") {return 27;}
-			return store.peekRecord('attribute', 27).get('Image_Path');
+            return answer + " whorled";
         case "Pyramidal":
 			if (namedArgs.usage === "1") {return 28;}
-			return store.peekRecord('attribute', 28).get('Image_Path');
+            return answer + " pyramidal";
         case "Conical":
 			if (namedArgs.usage === "1") {return 29;}
-			return store.peekRecord('attribute', 29).get('Image_Path');
+            return answer + " conical";
         case "Columnar":
 			if (namedArgs.usage === "1") {return 30;}
-			return store.peekRecord('attribute', 30).get('Image_Path');
+            return answer + " columnar";
         case "Spreading":
 			if (namedArgs.usage === "1") {return 31;}
-			return store.peekRecord('attribute', 31).get('Image_Path');
+            return answer + " spreading";
         case "Vase Shaped":
 			if (namedArgs.usage === "1") {return 32;}
-			return store.peekRecord('attribute', 32).get('Image_Path');
+            return answer + " vase";
         case "Broad":
 			if (namedArgs.usage === "1") {return 33;}
-			return store.peekRecord('attribute', 33).get('Image_Path');
+            return answer + " broad";
         case "Rounded":
 			if (namedArgs.usage === "1") {return 34;}
-			return store.peekRecord('attribute', 34).get('Image_Path');
+            return answer + " rounded";
         case "Cream":
 			if (namedArgs.usage === "1") {return 38;}
-			return store.peekRecord('attribute', 38).get('Image_Path');
+            return answer + " flowercream";
         case "White":
 			if (namedArgs.usage === "1") {return 39;}
-			return store.peekRecord('attribute', 39).get('Image_Path');
+            return answer + " flowerwhite";
         case "Pink":
 			if (namedArgs.usage === "1") {return 40;}
-			return store.peekRecord('attribute', 40).get('Image_Path');
+            return answer + " flowerpink";
         case "Purple":
 			if (namedArgs.usage === "1") {return 41;}
-			return store.peekRecord('attribute', 41).get('Image_Path');
+            return answer + " flowerpurple";
         case "Brown":
 			if (namedArgs.usage === "1") {return 42;}
-			return store.peekRecord('attribute', 42).get('Image_Path');
+            return answer + " flowerbrown";
         case "Pods":
 			if (namedArgs.usage === "1") {return 43;}
-			return store.peekRecord('attribute', 43).get('Image_Path');
+            return answer + " pod";
         case "Balls, Capsules, Tufted Fruit":
 			if (namedArgs.usage === "1") {return 44;}
-			return store.peekRecord('attribute', 44).get('Image_Path');
+            return answer + " ball";
         case "Acorns":
 			if (namedArgs.usage === "1") {return 45;}
-			return store.peekRecord('attribute', 45).get('Image_Path');
+            return answer + " acorn";
         case "Nuts":
 			if (namedArgs.usage === "1") {return 46;}
-			return store.peekRecord('attribute', 46).get('Image_Path');
+            return answer + " nut";
         case "Berrylike fruit":
 			if (namedArgs.usage === "1") {return 47;}
-			return store.peekRecord('attribute', 47).get('Image_Path');
+            return answer + " berry";
         case "Fleshy Fruit":
 			if (namedArgs.usage === "1") {return 48;}
-			return store.peekRecord('attribute', 48).get('Image_Path');
+            return answer + " fruit";
         case "Capsules":
 			if (namedArgs.usage === "1") {return 49;}
-			return store.peekRecord('attribute', 49).get('Image_Path');
+            return answer + " ball";
         case "Conelike":
 			if (namedArgs.usage === "1") {return 50;}
-			return store.peekRecord('attribute', 50).get('Image_Path');
+            return answer + " cone";
         case "Drupes":
 			if (namedArgs.usage === "1") {return 51;}
-			return store.peekRecord('attribute', 51).get('Image_Path');
+            return answer + " fruit";
         case "Plum":
 			if (namedArgs.usage === "1") {return 52;}
-			return store.peekRecord('attribute', 52).get('Image_Path');
+            return answer + " fruit";
         case "Berries":
 			if (namedArgs.usage === "1") {return 53;}
-			return store.peekRecord('attribute', 53).get('Image_Path');
+            return answer + " berry";
     }
 }
 
