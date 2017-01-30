b854c2f005c0845395e05129b1444838c89a0088
Alan Jiang <alanyimajiang@hotmail.com>
2017-01-30 17:21:30 -0500

    Working with hamburger menu.  There's an icon that does something, but I've hdiden it for the moment.  Media queries don't seem to work with ember.

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    title: 'Georgia Tech Arboretum'
+    title: 'Georgia Tech Arboretum',
+    actions: {
+        toggleMenu() {
+            var x = document.getElementById("menu");
+            if (x.className === "nav nav-pills pull-right") {
+                x.className += " responsive";
+            } else {
+                x.className = "nav nav-pills pull-right";
+            }
+        }
+    }
 });
